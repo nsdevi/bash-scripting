8021b16249dbe71d837cab132d6086e7ef884ffb
Mithrigit <nuthikattusowmithri969@gmail.com>
2022-09-11 13:20:57 +0530

    makefile

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -8,7 +8,7 @@ fi
 #Declaring the stat function
 
 stat() {
-    if [ $1 -eq 0 ] then
+    if [ $1 -eq 0 ]; then
         echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m failed, please check the logs \e[0m"
