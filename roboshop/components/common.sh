be9f5a068aef4144478864ec1bd6ad17aaf5f183
Mithrigit <nuthikattusowmithri969@gmail.com>
2022-09-11 12:52:22 +0530

    validating

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,5 +1,16 @@
+# validating whether the executing user is root or not
 ID=$(id -u)
 if [ $ID -ne 0 ]; then
-    echo -e "\e[31m Execute the script with the Sudo or rrot user"
+    echo -e "\e[31m Execute the script with the Sudo or root user \e[0m"
     exit 1
 fi
+
+#Declaring the stat function
+
+stat() {
+    if [ $1 -eq 0 ] then
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m failed, please check the logs \e[0m"
+    fi
+}
