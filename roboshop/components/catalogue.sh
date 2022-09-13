1f37cac78b623e5ab9a273166b0e3b379ed3127e
Mithrigit <nuthikattusowmithri969@gmail.com>
2022-09-13 14:10:41 +0530

    catalogue

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+set -e #ensure script will stop if any instruction fails
+
+source components/common.sh
+
+component=catalogue
+FUSER=roboshop
+
+echo -n "configuring repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+stat$?
+
+echo -n "installing nodejs"
+yum install nodejs -y
+
+echo "creating roboshop user"
+id $FUSER || useradd $FUSER 
+stat$?
+
+echo -n "Downloading component"
+curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${component}/archive/main.zip" >> /tmp/component.log
