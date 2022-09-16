61cc1704184974f5e4ca3b3280d670cb3d921983
Mithrigit <nuthikattusowmithri969@gmail.com>
2022-09-16 13:43:51 +0530

    frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -31,7 +31,7 @@ rm -rf frontend-main README.md
 echo "configuring the reverse proxy"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-for component in catalogue user cart shipping payment; do 
+for component in catalogue; do 
     echo -n "Updating the proxy file"
     sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
     stat $?
