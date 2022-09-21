9cbbce127763407433cd8e0f8c0dde6cd5243c11
Mithrigit <nuthikattusowmithri969@gmail.com>
2022-09-21 17:50:38 +0530

    aws

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -9,9 +9,9 @@ if [ -z  "$1" ] ; then
 fi 
 
 COMPONENT=$1
-ZONEID="Z04602961I29SHWLCRCU3"
+ZONEID="Z06382033OJK4C1D6WWYE"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID="sg-000671b0e1fb3d069"
+SGID="sg-0994b56a600ff5b9e"
 
 echo "The AMI which we are using is $AMI_ID"
 create-server() {
@@ -21,7 +21,7 @@ create-server() {
     echo "Spot Instance $COMPONENT is ready: "
     echo "Creating Route53 Record . . . . :"
 
-    sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json  >/tmp/record.json 
+    sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json  >/tmp/record.json
     aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file:///tmp/record.json | jq 
 }
 
