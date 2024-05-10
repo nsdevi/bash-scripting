98aff98086ef73af9de5175d1420a30bbbf90cea
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-10 16:38:16 +0530

    shebang notation

---
+++ 02.Print.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+echo line1
+echo line2   
+
+
+echo -e "line12\nline13"
+
+### -e indicates to display in new line 
+
+## #Escape Sequence Characters : 
+
+# \n : new line
+# \t : tab space 
