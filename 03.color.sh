5d045e46fdd64cc3aa8573f50c5e3809cb13cf02
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-10 16:47:45 +0530

    shebang notation

--- 03.color.sh
+++ 03.color.sh
@@ -5,5 +5,6 @@
 # Syntax is 
 # echo -e "\e[32m TextOfYourChoice  \e[0m"
 
-echo -e "\e[33m DevOps has a very great future \e[0m"
+echo -e "\033[33m DevOps has a very great future \033[0m"
 echo -e "\e[44;37m DevOps has a very great future \e[0m"
+## 44 in above line background color of the Text
