96024205391e7eb6e3dbd684a1cc0c2c17823de5
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-15 13:12:31 +0530

    shebang notation

--- 04.Variable.sh
+++ 04.Variable.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# No datatypes in Bashscripting, everyting is a String by default.
 a=10
 b=Hello
 
-## Echo is the cmd to print in bash
-## Bash= Bourne Again shell
-
+echo today's date is ${date+%F}
 echo value of a is :$a
 echo ${a}
 echo "${b}"
 echo value of d is :$d
-## if we don't declare the value, then by default it takes as Null value
 
+## if we don't declare the value, then by default it takes as Null value
 ### example "rm -rf Test/temp" it removes the temp
 ## rm -rf Test/${Dir} if we don't declare the DIr variable then it consider as Null and will remove the Test Directory
-
+# Local variables have more priority than environment variables
+# No datatypes in Bashscripting, everyting is a String by default.
+## Echo is the cmd to print in bash
+## Bash= Bourne Again shell
 
 
 ## All the scripts will be running on Heap Memory. If we are not sure of the value, we can pass through export while running(export d=10) and this value will be removed once machine got poweredoff/shutdown.
