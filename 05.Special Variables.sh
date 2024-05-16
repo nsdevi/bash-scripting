7fb2a89c81599671833dc63c0b9e9f692dcda120
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-16 12:22:37 +0530

    shebang notation

---
+++ 05.Special Variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# #  $0 --- It gives the name of the script you're running
+# #  $1 to $9---- you can pass maximum of 9 variables from the cmd line when running the script
+# #  $@
+# #  $*
+# #  $#
+# #  $$
+echo The script you're running is $0
+
+a=10
+
+echo The Value of a is :$a
+echo The value of b is :$b
+echo The value of C is :$C
+
