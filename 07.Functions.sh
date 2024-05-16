505f4e3eeb0b7feec9583c1cc9475be757e12192
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-16 14:24:16 +0530

    shebang notation

--- 07.Functions.sh
+++ 07.Functions.sh
@@ -6,6 +6,7 @@ stat()
 {
 echo Load average of the system from last 1 min is: $(uptime | awk -F : '{print $5}' | awk -F , '{print$1}')
 echo No.of Users logged in are: $(who | wc -l)
+echo $(cat 06.read.sh)
 }
 
 ##Calling the function
