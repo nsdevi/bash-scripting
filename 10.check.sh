e698f027f3064b7b1a42f9155f77fda35a26044f
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-21 13:34:40 +0530

    shebang notation

--- 10.check.sh
+++ 10.check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ID=$(id -u)
+ID=$(id -u) ###User id of the root user is always 0
 if [ $ID -eq 0 ]; then
     echo Installing HTTPD
     yum install httpd
 else
-    echo "Try to excute with the root uset or Sudo"
+    echo "Try to excute with the root user or Sudo"
 fi
