ac8141c332525e1876757e422d462e06c4a3b245
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-21 11:42:15 +0530

    shebang notation

--- 09.ifelse.sh
+++ 09.ifelse.sh
@@ -2,7 +2,7 @@
 
 a=$1
 if [a>=18]; then
-    echo "You are $a years old, eligible to cast a vote"
+    echo You are ${a} years old, eligible to cast a vote
 else
     echo "You are not eligible to cast a vote"
 fi
