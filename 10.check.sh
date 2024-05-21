2de7d649d05565084c53ee3fa04fc61a74e16d72
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-21 13:22:47 +0530

    shebang notation

--- 10.check.sh
+++ 10.check.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo Installing HTTPD
-yum install httpd
+ID=${id -u}
+if [ $ID -eq 0 ]; then
+    echo Installing HTTPD
+    yum install httpd
+else
+    echo "Try to excute with the root uset or Sudo"
+fi
