93d23972df7ff270321df223bac405f6cb064763
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-21 12:36:37 +0530

    shebang notation

--- 09.ifelse.sh
+++ 09.ifelse.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-a=$1
-if [ "$a" -ge "18" ] ; then
-    echo "You are ${a} years old, eligible to cast a vote"
-else
-    echo "You are not eligible to cast a vote"
-fi
+#Demo on IF
+# a=$1
+# if [ $a -ge 18 ] ; then
+#     echo "You are ${a} years old, eligible to cast a vote"
+# else
+#     echo "You are not eligible to cast a vote"
+# fi
 
 
-action=$2
-if [ "$action" = "start" ] ; then
-    echo Selected option is start
-else
-    echo Invalid Option
-fi
+# action=$2
+# if [ "$action" = "start" ] ; then
+#     echo Selected option is start
+# else
+#     echo Invalid Option
+# fi
+
+
+
+#Demo of ifelse
+action=$1
+if [ $action = start ]; then
+    echo "Starting the service"
+elif [$action = stop]; then
+    echo "stop the service"
+elif [$action = restart]; then
+    echo "Restarting the service"
+else   
+    echo -e "\e[31m Invalid Option \e[0m"
