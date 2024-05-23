1511b2120515619f91f3e10352cf8c6e244a2d79
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 13:06:42 +0530

    shebang notation

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-    echo "Try to execute with sudo or root user"
-    exit 10
-fi
+## source is the command to import the file
+source common.sh
 
 echo "Installing Nginx"
 yum install nginx -y
@@ -12,3 +8,10 @@ systemctl enable nginx
 systemctl start nginx
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.mdk
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
