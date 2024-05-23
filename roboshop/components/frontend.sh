2cb87bcf509927962de937f4068e7369f35dd308
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 14:53:44 +0530

    shebang notation

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,6 +7,8 @@ echo "Installing Nginx"
 yum install nginx -y >> /tmp/frontend.log
 if [ $? -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m "
+else
+    echo -e "\e[31m Failure \e[0m "
 systemctl enable nginx
 
 echo "Starting the Nginx"
