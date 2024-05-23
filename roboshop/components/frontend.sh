4d28aeb45dc53033a8d1dd15912de17ee04edf03
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 14:52:34 +0530

    shebang notation

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e #it ensure that script will stop if any instruction fails
 ## source is the command to import the file
 source components/common.sh
 
 echo "Installing Nginx"
 yum install nginx -y >> /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m "
 systemctl enable nginx
+
+echo "Starting the Nginx"
 systemctl start nginx
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
