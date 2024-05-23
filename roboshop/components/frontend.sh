88d3900b7afb3d76bdd9a45634e5ddf7b33ccf8b
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 14:11:58 +0530

    shebang notation

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,14 +3,14 @@
 source components/common.sh
 
 echo "Installing Nginx"
-yum install nginx -y
+yum install nginx -y >> /tmp/frontend.log
 systemctl enable nginx
 systemctl start nginx
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  >> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.mdk
