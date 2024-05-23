1c9035cc5ab6ef301d6007661cd05c6f50b08800
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 12:37:28 +0530

    shebang notation

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "I am installing frontend"
+echo "Installing Nginx"
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
