b9be4df856d25f371e1e116876911ccc45b9e2b6
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 12:44:31 +0530

    shebang notation

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ID= $(id -u)
+if [ $ID -ne 0 ] ; then
+    echo "Try to execute with sudo or root user"
+    exit 1
+fi
+
 echo "Installing Nginx"
 yum install nginx -y
 systemctl enable nginx
