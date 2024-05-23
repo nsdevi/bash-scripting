cc5fb7ea061df2380cd739ef131cb4637788439d
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-23 13:08:51 +0530

    shebang notation

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,6 +1,6 @@
 ## VAlidating whether the executing user is root or not
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m Try to execute with sudo or root user \e[0m]"
+    echo -e "\e[31m Try to execute with sudo or root user \e[0m"
     exit 1
 fi
