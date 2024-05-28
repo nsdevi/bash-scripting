58283e7ab2c96ec2b94b79bec8c8925260633de2
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-28 15:41:49 +0530

    shebang notation

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,74 +1,74 @@
 #!/bin/bash
-# set -e
-# source components/catalogue.sh
-# COMPONENT=catalogue
-# FUSER=roboshop
-
-# echo -n "Downloading yum repos for Nodejs"
-# curl -sL https://rpm.nodesource.com/setup_16.x | bash
-# stat $?
-
-# echo -n "Installing NodeJS"
-# yum install nodejs -y
-# stat $?
-
-# echo -n "$FUSER User Creation"
-# useradd ${FUSER}
-# stat $?
-
-# echo -n "Downloading $COMPONENT"
-# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-# stat $?
-
-# echo -n "Cleaning up old $COMPONENT files"
-# rm -rf /home/${FUSER}/${COMPONENT}
-# stat $?
-
-# echo -n "Extracting the $COMPONENT schema"
-# cd /home/${FUSER}
-# unzip -o /tmp/${COMPONENT}.zip ## "-o indicates it overrides the file"
-# mv ${COMPONENT}-main ${COMPONENT}
-# stat $?
-
-# echo -n "Installing $COMPONENT dependecies"
-# npm install
-# stat $?
-
-
-
-
-
-set -e 
-
-source components/common.sh
-
+set -e
+source components/catalogue.sh
 COMPONENT=catalogue
+FUSER=roboshop
 
-echo -n "Configure Yum Remos for nodejs:"
-curl -sL https://rpm.nodesource.com/setup_16.x | bash >> /tmp/${COMPONENT}.log 
+echo -n "Downloading yum repos for Nodejs"
+curl -sL https://rpm.nodesource.com/setup_16.x | bash
 stat $?
 
-echo -n "Installing nodejs:"
-yum install nodejs -y >> /tmp/${COMPONENT}.log 
-stat $? 
-
-echo -n "Adding $FUSER user:"
-id ${FUSER} &>> LOGFILE  || useradd ${FUSER}   # Creates users only in case if the user account doen's exist
+echo -n "Installing NodeJS"
+yum install nodejs -y
 stat $?
 
-echo -n "Downloading ${COMPONENT} :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
-stat $? 
-
-echo -n "Cleanup of Old ${COMPONENT} content:"
-rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+echo -n "$FUSER User Creation"
+useradd ${FUSER}
 stat $?
 
-echo -n "Extracting ${COMPONENT} content: "
-cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
-unzip -o  /tmp/${COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
-stat $? 
-
-echo -n "Changing the ownership to ${FUSER}:"
-chown -R $FUSER:$FUSER $COMPONENT/
+echo -n "Downloading $COMPONENT"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
+
+echo -n "Cleaning up old $COMPONENT files"
+rm -rf /home/${FUSER}/${COMPONENT}
+stat $?
+
+echo -n "Extracting the $COMPONENT schema"
+cd /home/${FUSER}
+unzip -o /tmp/${COMPONENT}.zip ## "-o indicates it overrides the file"
+mv ${COMPONENT}-main ${COMPONENT}
+stat $?
+
+echo -n "Installing $COMPONENT dependecies"
+npm install
+stat $?
+
+
+
+
+
+# set -e 
+
+# source components/common.sh
+
+# COMPONENT=catalogue
+
+# echo -n "Configure Yum Remos for nodejs:"
+# curl -sL https://rpm.nodesource.com/setup_16.x | bash >> /tmp/${COMPONENT}.log 
+# stat $?
+
+# echo -n "Installing nodejs:"
+# yum install nodejs -y >> /tmp/${COMPONENT}.log 
+# stat $? 
+
+# echo -n "Adding $FUSER user:"
+# id ${FUSER} &>> LOGFILE  || useradd ${FUSER}   # Creates users only in case if the user account doen's exist
+# stat $?
+
+# echo -n "Downloading ${COMPONENT} :"
+# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
+# stat $? 
+
+# echo -n "Cleanup of Old ${COMPONENT} content:"
+# rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+# stat $?
+
+# echo -n "Extracting ${COMPONENT} content: "
+# cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
+# unzip -o  /tmp/${COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
+# stat $? 
+
+# echo -n "Changing the ownership to ${FUSER}:"
+# chown -R $FUSER:$FUSER $COMPONENT/
+# stat $?
