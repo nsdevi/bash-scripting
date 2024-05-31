9b31c085cdfc60ea6eb0c03c13dbe1827adceaee
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-05-31 12:10:20 +0530

    shebang notation

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -9,5 +9,14 @@ curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.r
 stat $?
 echo -n "Installing $COMPONENT"
 yum install redis-6.2.13 -y >> /tmp/$COMPONENT.log
+stat $? 
+
+echo -n "whitelisting the $COMPONENT config"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/$COMPONENT.conf
+stat $?
+
+echo -n "Starting $COMPONENT"
+systemctl enable redis >> /tmp/$COMPONENT.log
+systemctl start redis >> /tmp/$COMPONENT.log
 stat $?
 
