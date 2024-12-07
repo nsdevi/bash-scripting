2a5e62a4f6a2b2176d2051b247b64c2863f03024
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-12-07 21:17:44 +0530

    shebang notation

--- roboshop_v1/components/common.sh
+++ roboshop_v1/components/common.sh
@@ -1,4 +1,7 @@
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then
-echo -e "/e[31m Try to execute with sudo/root access /e[0m"
+    echo -e "\e[31m Try to execute with sudo/root access \e[0m"
+    exit 1
 fi
+
+
