783d549261813a18d718f99d21f0d66168709d70
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-12-07 21:31:16 +0530

    shebang notation

--- roboshop_v1/components/frontend.sh
+++ roboshop_v1/components/frontend.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-#source components/common.sh
+source components/common.sh
 echo "I'm installing frontend"
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
