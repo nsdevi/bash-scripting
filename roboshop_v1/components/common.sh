a158a44dd7d0c03a8857151a5b5f8105689c47d6
Sowmithri Devi <User@DESKTOP-0EIBJJV>
2024-12-10 20:21:29 +0530

    shebang notation

--- roboshop_v1/components/common.sh
+++ roboshop_v1/components/common.sh
@@ -5,7 +5,7 @@ if [ $ID -ne 0 ] ; then
 fi
 
 stat() {
-    if [ $1 -eq 0 ]; then
+    if [ $1 -eq 0 ] ; then
     echo -e "\e[32m Success \e[0m"
     else
     echo -e "\e[31m Failed, check for the logs \e[0m"
